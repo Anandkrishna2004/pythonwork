66dc1fcb856f6d0dba77125617c72a963bf5cd3f
Anandkrishna2004 <aanandkrishnaa01@gmail.com>
2025-03-26 12:35:02 +0530

    Add files via upload

---
+++ 1st name last name in function.py
@@ -0,0 +1,13 @@
+def ins():
+    name=input("Enter your name")
+    last_name=input("Enter last name")
+    item=name+last_name
+    print(item)
+ins()
+
+def my_function():
+    name=input("Enter your name")
+    last_name=input("Enter last name")
+    item=name+last_name
+    print(item)
+my_function()
